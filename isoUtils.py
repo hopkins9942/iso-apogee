8d61d98b203091513fbf5cbe26ef282d7b773316
Matthew Hopkins <matthew.hopkins9942@gmail.com>
2022-08-16 17:09:47 +0100

    makeBins done

---
+++ isoUtils.py
@@ -0,0 +1,38 @@
+import numpy as np
+import astropy.coordinates as coord
+import astropy.units as u
+
+
+# paths - edit to match machines used
+# Assumes makeBins.py, calcEffSelFunct.py and doFit.py are run on a cluster,
+# data directory is scp-ed to local machine then analysis.py is run there
+clusterDataDir = '/data/phys-galactic-isos/sjoh4701/APOGEE/' # used by makeBins.py, calcEffSelFunct.py and doFit.py
+localDataDir = '/Users/hopkinsm/data/' # used by analysis.py
+
+GC_frame = coord.Galactocentric() #adjust parameters here if needed
+z_Sun = GC_frame.z_sun.to(u.kpc).value # .value removes unit, which causes problems with pytorch
+R_Sun = np.sqrt(GC_frame.galcen_distance.to(u.kpc).value**2 - z_Sun**2)
+
+muMin = 4.0
+muMax = 17.0
+muStep = 0.1
+muGridParams = (muMin, muMax, muStep)
+
+def arr(gridParams):
+    start, stop, step = gridParams
+    arr = np.arange(round((stop-start)/step)+1)*step+start
+    assert isclose(arr[-1],stop) # will highlight both bugs and when stop-start is not multiple of diff
+    return arr
+
+def binName(binDict):
+    binDict = dict(sorted(binDict.items()))
+    # ensures same order independant of construction. I think sorts according to ASCII value of first character of key
+    return '_'.join(['_'.join([key, f'{limits[0]:.3f}', f'{limits[1]:.3f}']) for key,limits in binDict.items()])
+    #Note: python list comprehensions are cool
+
+_FeH_edges = arr((-1.575, 0.625, 0.1)) #0.625-09.725 has no APOGEE statsample stars, -1.575--1.475 has about 130
+_age_edges = np.array([-999.0,4.5,9.0,999.0]) # remember ages only good for FeH>-0.5
+_FeH_edges_for_age = arr((-0.475, 0.625, 0.1))
+binsToUse = [{'FeH': (_FeH_edges[i], _FeH_edges[i+1])} for i in range(len(_FeH_edges)-1)]
+#binsToUse = [{'FeH': (_FeH_edges_for_age[i], _FeH_edges_for_age[i+1]), 'age': (_age_edges[j], _age_edges[j+1])}
+#    for i in range(len(_FeH_edges_for_age)-1) for j in range(len(_age_edges)-1)]
---
+++ pickleGetters.py
@@ -0,0 +1,82 @@
+import os
+import pickle
+
+import apogee.select as apsel
+import apogee.tools.read as apread
+
+import isoUtils
+
+# each funct either loads file or calculates and saves it if it is not created already. onCluster affects only where files are looked for, I don't expect to use this
+
+if __name__ == '__main__':
+    # call module as script to ensure all pickles are created
+    get_apo()
+    get_allStar()
+    get_allStar_statIndx()
+
+
+def get_apo(onCluster=True):
+    """
+    """
+    path = os.path.join(isoUtils.clusterDataDir if onCluster else
+                        isoUtils.localDataDir, 'input_data', 'apodr16_csf.dat')
+    if os.path.exists(path):
+        with open(path, 'rb') as f:
+            apo = pickle.load(f)
+    else:
+        apo = apsel.apogeeCombinedSelect()
+        with open(path, 'wb') as f:
+            pickle.dump(apo, f)
+    # maybe add del line here or later if I have memory problems
+    return apo
+
+def get_allStar(onCluster=True):
+    """
+    uses a pickle with options:
+        rmcommissioning=True,
+        main=True,
+        exclude_star_bad=True,
+        exclude_star_warn=True,
+        use_astroNN_distances=True,
+        use_astroNN_ages=True,
+        rmdups=True
+    """
+    path = os.path.join(isoUtils.clusterDataDir if onCluster else
+                        isoUtils.localDataDir, 'input_data', 'dr16allStar.dat')
+    if os.path.exists(path):
+        with open(path, 'rb') as f:
+            allStar = pickle.load(f)
+    else:
+        print('WARNING: calculating allStar')
+        allStar = apread.allStar(
+            rmcommissioning=True,
+            main=True,
+            exclude_star_bad=True,
+            exclude_star_warn=True,
+            use_astroNN_distances=True,
+            use_astroNN_ages=True,
+            rmdups=True)
+        with open(path, 'wb') as f:
+            pickle.dump(allStar, f)
+    assert len(allStar) == 211051
+    return allStar
+
+def get_allStar_statIndx(onCluster=True):
+    """
+    returns both as whenever statIndx is needed, allStar is too
+    """
+    allStar = get_allStar()
+
+    statIndxPath = os.path.join(isoUtils.clusterDataDir if onCluster else
+                        isoUtils.localDataDir, 'input_data', 'dr16statIndx.dat')
+    if os.path.exists(statIndxPath):
+        with open(statIndxPath, 'rb') as f:
+            statIndx = pickle.load(f)
+    else:
+        print('WARNING: calculating statIndx')
+        apo = get_apo()
+        statIndx = apo.determine_statistical(allStar)
+        with open(path, 'wb') as f:
+            pickle.dump(statIndx, f)
+    assert np.count_nonzero(statIndx)==165768
+    return (allStar, statIndx)
